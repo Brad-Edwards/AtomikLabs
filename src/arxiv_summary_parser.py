07f075d19488f83b64bd1e68fad7aa4a3344c00d
Brad Edwards <j.bradley.edwards@gmail.com>
2023-10-25 21:39:32 -0700

    Add better code organization for readability, comments, refactor to remove global

--- src/arxiv_summary_parser.py
+++ src/arxiv_summary_parser.py
@@ -1,35 +1,11 @@
+""" This module is responsible for parsing arXiv daily summaries and extracting relevant data. """
+
 import json
 from collections import defaultdict
 import xml.etree.ElementTree as ET
-
 import boto3
-import json
 
-
-def lambda_handler(event: dict, context) -> dict:
-    """
-    Parses arXiv daily summaries from the specified date and uploads them to S3.
-
-    Args:
-        event (dict): The event data passed to the function.
-        context (LambdaContext): The context in which the function is called.
-
-    Returns:
-        dict: A dict with a status code and message.
-    """
-
-    #xml_data = event.get("xml_data")
-    #from_date = event.get("from_date")
-    print(event)
-
-    #extracted_data_chunk = parse_xml_data(xml_data, from_date)
-
-    #return {
-    #    "statusCode": 200,
-    #    "body": f"Successfully parsed arXiv daily summaries from {from_date}",
-    #    "extracted_data_chunk": extracted_data_chunk
-    #}
-
+from package.botocore.client import BaseClient
 
 cs_categories_inverted = {
     'Computer Science - Artifical Intelligence': 'AI',
@@ -75,53 +51,51 @@ cs_categories_inverted = {
 }
 
 
-def parse_xml_data(xml_data: str, from_date: str) -> dict:
+def fetch_s3_object(client: BaseClient, bucket: str, key: str) -> str:
+    """
+    Fetches an object from S3.
+
+    Args:
+        client (BaseClient): The S3 client.
+        bucket (str): The name of the bucket.
+        key (str): The key of the object.
+
+    Returns:
+        str: The object data.
+    """
+    s3 = client
+
+    response = s3.get_object(Bucket=bucket, Key=key)
+    data_list = json.loads(response['Body'].read().decode('utf-8'))
+
+    if isinstance(data_list, list):
+        return "".join(data_list)
+
+    return data_list
+
+
+def parse_xml_data(xml_data: str) -> dict:
+    """
+    Parses XML data and extracts relevant data.
+
+    Args:
+        xml_data (str): XML data to parse.
+
+    Returns:
+        dict: A dict with parsed data.
+    """
     extracted_data_chunk = defaultdict(list)
 
     try:
         root = ET.fromstring(xml_data)
-        ns = {
-            'oai': 'http://www.openarchives.org/OAI/2.0/',
-            'dc': 'http://purl.org/dc/elements/1.1/'
-        }
+        ns = {'oai': 'http://www.openarchives.org/OAI/2.0/', 'dc': 'http://purl.org/dc/elements/1.1/'}
 
         for record in root.findall(".//oai:record", ns):
             date_elements = record.findall(".//dc:date", ns)
             if len(date_elements) != 1:
                 continue
 
-            identifier = record.find(".//oai:identifier", ns).text
-            abstract_url = record.find(".//dc:identifier", ns).text
-
-            creators_elements = record.findall(".//dc:creator", ns)
-            authors = []
-            for creator in creators_elements:
-                name_parts = creator.text.split(", ", 1)
-                last_name = name_parts[0]
-                first_name = name_parts[1] if len(name_parts) > 1 else ""
-                authors.append({'last_name': last_name, 'first_name': first_name})
-
-            subjects_elements = record.findall(".//dc:subject", ns)
-            categories = [cs_categories_inverted.get(subject.text, "") for subject in subjects_elements]
-
-            primary_category = categories[0] if categories else ""
-
-            abstract = record.find(".//dc:description", ns).text
-            title = record.find(".//dc:title", ns).text
-            date = date_elements[0].text
-            group = 'cs'
-
-            extracted_data_chunk['records'].append({
-                'identifier': identifier,
-                'abstract_url': abstract_url,
-                'authors': authors,
-                'primary_category': primary_category,
-                'categories': categories,  # All categories
-                'abstract': abstract,
-                'title': title,
-                'date': date,
-                'group': group
-            })
+            extracted_data_chunk['records'].append(extract_record_data(record, ns))
 
     except ET.ParseError as e:
         print(f"Parse error: {e}")
@@ -129,8 +103,137 @@ def parse_xml_data(xml_data: str, from_date: str) -> dict:
     return extracted_data_chunk
 
 
+def extract_record_data(record, ns: dict) -> dict:
+    """
+    Extracts relevant data from an arXiv research summary record.
+
+    Args:
+        record (ET.Element): The record element.
+        ns (dict): A dict of namespaces.
+
+    Returns:
+        dict: A dict with extracted data.
+    """
+    identifier = record.find(".//oai:identifier", ns).text
+    abstract_url = record.find(".//dc:identifier", ns).text
+    authors = extract_authors(record, ns)
+    categories = extract_categories(record, ns)
+    primary_category = categories[0] if categories else ""
+    abstract = record.find(".//dc:description", ns).text
+    title = record.find(".//dc:title", ns).text
+    date = record.find(".//dc:date", ns).text
+
+    return {
+        'identifier': identifier,
+        'abstract_url': abstract_url,
+        'authors': authors,
+        'primary_category': primary_category,
+        'categories': categories,
+        'abstract': abstract,
+        'title': title,
+        'date': date,
+        'group': 'cs'
+    }
+
+
+def extract_authors(record, ns: dict) -> list:
+    """
+    Extracts authors from an arXiv research summary record.
+
+    Args:
+        record (ET.Element): The record element.
+        ns (dict): A dict of namespaces.
+
+    Returns:
+        list: A list of authors.
+    """
+    authors = []
+    creators_elements = record.findall(".//dc:creator", ns)
+
+    for creator in creators_elements:
+        name_parts = creator.text.split(", ", 1)
+        last_name = name_parts[0]
+        first_name = name_parts[1] if len(name_parts) > 1 else ""
+        authors.append({'last_name': last_name, 'first_name': first_name})
+
+    return authors
+
+
+def extract_categories(record, ns: dict) -> list:
+    """
+    Extracts categories from an arXiv research summary record.
+
+    Args:
+        record (ET.Element): The record element.
+        ns (dict): A dict of namespaces.
+
+    Returns:
+        list: A list of categories.
+    """
+    subjects_elements = record.findall(".//dc:subject", ns)
+    return [cs_categories_inverted.get(subject.text, "") for subject in subjects_elements]
+
+
+def upload_to_s3(client: BaseClient, data: dict, key: str) -> None:
+    """
+    Uploads data to S3.
+
+    Args:
+        client (BaseClient): The S3 client.
+        data (dict): The data to upload.
+        key (str): The key of the object.
+
+    Returns:
+        None
+    """
+    s3 = client
+    bucket_name = "techcraftingai-data-processing"
+    object_name = f"arxiv/{key}-parsed.json"
+    s3.put_object(
+        Body=json.dumps(data),
+        Bucket=bucket_name,
+        Key=object_name,
+        ContentType='application/json'
+    )
+
+
+def lambda_handler(event: dict, context) -> dict:
+    """
+    The main entry point for the Lambda function.
+
+    Args:
+        event (dict): The event data.
+        context: The context data.
+
+    Returns:
+        dict: A dict with the status code and body.
+    """
+    s3 = boto3.client("s3")
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    xml_data = fetch_s3_object(s3, bucket, key)
+    extracted_data_chunk = parse_xml_data(xml_data)
+    upload_to_s3(s3, extracted_data_chunk, key)
+
+    return {
+        "statusCode": 200,
+        "body": "Successfully parsed arXiv daily summaries"
+    }
+
+
 if __name__ == "__main__":
-    lambda_handler({
-        "xml_data": "",
-        "from_date": "2021-10-24"
-    }, None)
+    test_event = {
+        "Records": [
+            {
+                "s3": {
+                    "bucket": {
+                        "name": "techcraftingai-inbound-data"
+                    },
+                    "object": {
+                        "key": "arxiv/cs-2023-10-24.json"
+                    }
+                }
+            }
+        ]
+    }
+    lambda_handler(test_event, None)
